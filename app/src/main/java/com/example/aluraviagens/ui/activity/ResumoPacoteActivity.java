ed8cdca873ad297f1efd95f3a5648f49dae27a33
(VA) João Guilherme Lima <joaolima@cta.com.br>
2022-02-21 12:43:24 -0300

    Added trip data information based on current date.

--- app/src/main/java/com/example/aluraviagens/ui/activity/ResumoPacoteActivity.java
+++ app/src/main/java/com/example/aluraviagens/ui/activity/ResumoPacoteActivity.java
@@ -14,6 +14,8 @@ import com.example.aluraviagens.util.MoedaUtil;
 import com.example.aluraviagens.util.ResourceUtil;
 
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class ResumoPacoteActivity extends AppCompatActivity {
 
@@ -21,6 +23,7 @@ public class ResumoPacoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_pacote);
+        setTitle("Resumo do pacote");
 
         Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp",
                 2, new BigDecimal("243.99"));
@@ -41,6 +44,18 @@ public class ResumoPacoteActivity extends AppCompatActivity {
         String moedaBrasileira = MoedaUtil
                 .formataBrasileiro(pacoteSaoPaulo.getPreco());
         preco.setText(moedaBrasileira);
+
+        TextView data = findViewById(R.id.resumo_pacote_data);
+        Calendar dataIda = Calendar.getInstance();
+        Calendar dataVolta = Calendar.getInstance();
+        dataVolta.add(Calendar.DATE, pacoteSaoPaulo.getDias());
+        SimpleDateFormat formatoBrasileiro = new SimpleDateFormat("dd/MM");
+        String dataFormatadaIda = formatoBrasileiro.format(dataIda.getTime());
+        String dataFormatadaVolta = formatoBrasileiro.format(dataVolta.getTime());
+
+        String dataFormatadaViagem = dataFormatadaIda + " - " + dataFormatadaVolta + " de " + dataVolta.get(Calendar.YEAR);
+        data.setText(dataFormatadaViagem);
+
     }
 
 }
